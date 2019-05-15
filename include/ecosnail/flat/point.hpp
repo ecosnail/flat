82e5dff47a79a1a93072eb7ad5f4f10f314f3604
snailbaron <murbidodrus@gmail.com>
2019-05-16 00:26:39 +0300

    Add Point and Vector prototypes

---
+++ include/ecosnail/flat/point.hpp
@@ -0,0 +1,190 @@
+#pragma once
+
+#include <ecosnail/flat/vector.hpp>
+
+#include <cassert>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+namespace ecosnail::flat {
+
+template <class T>
+struct Point {
+    // construction
+
+    Point()
+        : x{}, y{}
+    { }
+
+    explicit Point(T x, T y)
+        : x(std::move(x)), y(std::move(y))
+    { }
+
+    // implicit conversions
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Point(const Point<U>& rhs)
+        : x(rhs.x), y(rhs.y)
+    { }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Point(Point<U>&& rhs)
+        : x(std::move(rhs.x)), y(std::move(rhs.y))
+    { }
+
+    // assignment
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Point& operator=(const Point<U>& rhs)
+    {
+        x = rhs.x;
+        y = rhs.y;
+        return *this;
+    }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Point& operator=(const Point<U>&& rhs)
+    {
+        x = std::move(rhs.x);
+        y = std::move(rhs.y);
+        return *this;
+    }
+
+    // array subscript operator
+
+    T& operator[](std::size_t idx)
+    {
+        assert(idx < 2);
+        static const T Point::* ptrs[] {&Point::x, &Point::y};
+        return this->*ptrs[idx];
+    }
+
+    const T& operator[](std::size_t idx) const
+    {
+        assert(idx < 2);
+        static const T Point::* ptrs[] {&Point::x, &Point::y};
+        return this->*ptrs[idx];
+    }
+
+    // arithmetic operators
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Point& operator+=(const Vector<U>& rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Point& operator-=(const Vector<U>& rhs)
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        return *this;
+    }
+
+    T x;
+    T y;
+};
+
+// arithmetic operators
+
+template <class L, class R>
+Point<std::common_type_t<L, R>> operator+(
+    const Point<L>& lhs, const Vector<R>& rhs)
+{
+    return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+template <class L, class R>
+Point<std::common_type_t<L, R>> operator-(
+    const Point<L>& lhs, const Vector<R>& rhs)
+{
+    return {lhs.x - rhs.x, lhs.y - rhs.y};
+};
+
+// relational operators
+
+template <class T>
+bool operator==(const Point<T>& lhs, const Point<T>& rhs)
+{
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <class T>
+bool operator!=(const Point<T>& lhs, const Point<T>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template <class T>
+bool operator<=(const Point<T>& lhs, const Point<T>& rhs)
+{
+    return lhs.x <= rhs.x && lhs.y <= rhs.y;
+}
+
+template <class T>
+bool operator>=(const Point<T>& lhs, const Point<T>& rhs)
+{
+    return rhs <= lhs;
+}
+
+template <class T>
+bool operator<(const Point<T>& lhs, const Point<T>& rhs)
+{
+    return lhs <= rhs && lhs != rhs;
+}
+
+template <class T>
+bool operator>(const Point<T>& lhs, const Point<T>& rhs)
+{
+    return rhs < lhs;
+}
+
+} // namespace ecosnail::flat
+
+namespace std {
+
+template <class T>
+struct less<ecosnail::flat::Point<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Point<T>& lhs,
+        const ecosnail::flat::Point<T>& rhs)
+    {
+        return std::tie(lhs.x, lhs.y) < std::tie(rhs.x, rhs.y);
+    }
+};
+
+template <class T>
+struct greater<ecosnail::flat::Point<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Point<T>& lhs,
+        const ecosnail::flat::Point<T>& rhs)
+    {
+        return less<ecosnail::flat::Point<T>>(rhs, lhs)();
+    }
+};
+
+template <class T>
+struct less_equal<ecosnail::flat::Point<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Point<T>& lhs,
+        const ecosnail::flat::Point<T>& rhs)
+    {
+        return !greater<ecosnail::flat::Point<T>>(lhs, rhs);
+    }
+};
+
+template <class T>
+struct greater_equal<ecosnail::flat::Point<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Point<T>& lhs,
+        const ecosnail::flat::Point<T>& rhs)
+    {
+        return !less<ecosnail::flat::Point<T>>(lhs, rhs);
+    }
+};
+
+} // namespace ecosnail::flat
---
+++ include/ecosnail/flat/vector.hpp
@@ -0,0 +1,227 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <functional>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+namespace ecosnail::flat {
+
+template <class T>
+struct Vector {
+    // construction
+
+    Vector()
+        : x{}, y{}
+    { }
+
+    explicit Vector(T x, T y)
+        : x(std::move(x)), y(std::move(y))
+    { }
+
+    // implicit conversions
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector(const Vector<U>& rhs)
+        : x(rhs.x), y(rhs.y)
+    { }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector(Vector<U>&& rhs)
+        : x(std::move(rhs.x)), y(std::move(rhs.y))
+    { }
+
+    // assignment
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector& operator=(const Vector<U>& rhs)
+    {
+        x = rhs.x;
+        y = rhs.y;
+        return *this;
+    }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector& operator=(const Vector<U>&& rhs)
+    {
+        x = std::move(rhs.x);
+        y = std::move(rhs.y);
+        return *this;
+    }
+
+    // array subscript operator
+
+    T& operator[](std::size_t idx)
+    {
+        assert(idx < 2);
+        static const T Vector::* ptrs[] {&Vector::x, &Vector::y};
+        return this->*ptrs[idx];
+    }
+
+    const T& operator[](std::size_t idx) const
+    {
+        assert(idx < 2);
+        static const T Vector::* ptrs[] {&Vector::x, &Vector::y};
+        return this->*ptrs[idx];
+    }
+
+    // arithmetic operators
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector& operator+=(const Vector<U>& rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector& operator-=(const Vector<U>& rhs)
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        return *this;
+    }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector& operator*=(const U& scalar)
+    {
+        x *= scalar;
+        y *= scalar;
+        return *this;
+    }
+
+    template <class U, class = std::enable_if_t<std::is_convertible_v<U, T>>>
+    Vector& operator/=(const U& scalar)
+    {
+        x /= scalar;
+        y /= scalar;
+        return *this;
+    }
+
+    T x;
+    T y;
+};
+
+// arithmetic operators
+
+template <class L, class R>
+Vector<std::common_type_t<L, R>> operator+(
+    const Vector<L>& lhs, const Vector<R>& rhs)
+{
+    return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+template <class L, class R>
+Vector<std::common_type_t<L, R>> operator-(
+    const Vector<L>& lhs, const Vector<R>& rhs)
+{
+    return {lhs.x - rhs.x, lhs.y - rhs.y};
+};
+
+template <class T, class U>
+Vector<std::common_type_t<T, U>> operator*(
+    const Vector<T>& vector, const U& scalar)
+{
+    return {vector.x * scalar, vector.y * scalar};
+}
+
+template <class T, class U>
+Vector<std::common_type_t<T, U>> operator*(
+    const U& scalar, const Vector<T>& vector)
+{
+    return vector * scalar;
+}
+
+template <class T, class U>
+Vector<std::common_type_t<T, U>> operator/(
+    const Vector<T>& vector, const U& scalar)
+{
+    return {vector.x / scalar, vector.y / scalar};
+}
+
+// relational operators
+
+template <class T>
+bool operator==(const Vector<T>& lhs, const Vector<T>& rhs)
+{
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+template <class T>
+bool operator!=(const Vector<T>& lhs, const Vector<T>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template <class T>
+bool operator<=(const Vector<T>& lhs, const Vector<T>& rhs)
+{
+    return lhs.x <= rhs.x && lhs.y <= rhs.y;
+}
+
+template <class T>
+bool operator>=(const Vector<T>& lhs, const Vector<T>& rhs)
+{
+    return rhs <= lhs;
+}
+
+template <class T>
+bool operator<(const Vector<T>& lhs, const Vector<T>& rhs)
+{
+    return lhs <= rhs && lhs != rhs;
+}
+
+template <class T>
+bool operator>(const Vector<T>& lhs, const Vector<T>& rhs)
+{
+    return rhs < lhs;
+}
+
+} // namespace ecosnail::flat
+
+namespace std {
+
+template <class T>
+struct less<ecosnail::flat::Vector<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Vector<T>& lhs,
+        const ecosnail::flat::Vector<T>& rhs)
+    {
+        return std::tie(lhs.x, lhs.y) < std::tie(rhs.x, rhs.y);
+    }
+};
+
+template <class T>
+struct greater<ecosnail::flat::Vector<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Vector<T>& lhs,
+        const ecosnail::flat::Vector<T>& rhs)
+    {
+        return less<ecosnail::flat::Vector<T>>(rhs, lhs)();
+    }
+};
+
+template <class T>
+struct less_equal<ecosnail::flat::Vector<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Vector<T>& lhs,
+        const ecosnail::flat::Vector<T>& rhs)
+    {
+        return !greater<ecosnail::flat::Vector<T>>(lhs, rhs);
+    }
+};
+
+template <class T>
+struct greater_equal<ecosnail::flat::Vector<T>> {
+    constexpr bool operator()(
+        const ecosnail::flat::Vector<T>& lhs,
+        const ecosnail::flat::Vector<T>& rhs)
+    {
+        return !less<ecosnail::flat::Vector<T>>(lhs, rhs);
+    }
+};
+
+} // namespace std
